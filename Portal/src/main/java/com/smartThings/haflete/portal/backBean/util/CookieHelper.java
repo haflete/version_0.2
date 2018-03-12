ec1fc13c4afc9727544354f38a66d6acd8ff5ad0
haflete <wwwnedal@live.com>
2018-03-12 09:50:14 +0200

    haflete CookieHelper

---
+++ Portal/src/main/java/com/smartThings/haflete/portal/backBean/util/CookieHelper.java
@@ -0,0 +1,68 @@
+package com.smartThings.haflete.portal.backBean.util;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.Dependent;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Dependent
+public class CookieHelper implements Serializable{
+	
+	private static final long serialVersionUID = -1633984759378818888L;
+
+	public void setCookie(String name, String value, int expiry) {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+		Cookie cookie = null;
+
+		Cookie[] userCookies = request.getCookies();
+		if (userCookies != null && userCookies.length > 0) {
+			for (int i = 0; i < userCookies.length; i++) {
+				if (userCookies[i].getName().equals(name)) {
+					cookie = userCookies[i];
+					break;
+				}
+			}
+		}
+
+		if (cookie != null) {
+			cookie.setValue(value);
+		} else {
+			cookie = new Cookie(name, value);
+			cookie.setPath(request.getContextPath());
+		}
+
+		cookie.setMaxAge(expiry);
+
+		HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
+		response.addCookie(cookie);
+	}
+	
+	public void removeCookie(String name) {
+		setCookie(name, null, 0);
+	}
+
+	public Cookie getCookie(String name) {
+
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+
+		HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+		Cookie cookie = null;
+
+		Cookie[] userCookies = request.getCookies();
+		if (userCookies != null && userCookies.length > 0) {
+			for (int i = 0; i < userCookies.length; i++) {
+				if (userCookies[i].getName().equals(name)) {
+					cookie = userCookies[i];
+					return cookie;
+				}
+			}
+		}
+		return null;
+	}
+}
